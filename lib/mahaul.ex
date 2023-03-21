d3920d7017562e981533aee92d4802cf9d601654
~eMAD~ <emad.alam@yahoo.com>
2023-03-21 19:29:40 +0100

    chore: bump version

--- lib/mahaul.ex
+++ lib/mahaul.ex
@@ -2,7 +2,7 @@ defmodule Mahaul do
   @external_resource "README.md"
   @moduledoc File.read!("README.md") |> String.replace("# Mahaul\n\n", "", global: false)
 
-  @version "0.5.0"
+  @version "0.6.0"
 
   @doc false
   def version, do: @version
